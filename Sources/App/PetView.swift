cb256344a763c5abc63111235e15bbb29fc97f7f
babyskill <trungkientn@gmail.com>
2026-06-20 07:16:17 +0700

    fix(bubble): correct quote bubble arrow direction to point downward

--- Sources/App/PetView.swift
+++ Sources/App/PetView.swift
@@ -157,9 +157,9 @@ struct QuoteBubble: View {
 private struct Triangle: Shape {
     func path(in rect: CGRect) -> Path {
         var p = Path()
-        p.move(to: CGPoint(x: rect.minX, y: rect.maxY))
-        p.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        p.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        p.move(to: CGPoint(x: rect.minX, y: rect.minY))
+        p.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+        p.addLine(to: CGPoint(x: rect.midX, y: rect.maxY))
         p.closeSubpath()
         return p
     }
